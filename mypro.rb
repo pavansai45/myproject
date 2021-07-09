3412f3d9fc8223aa23c6c11a749d86df49534d32
pavansai45 <pavansaijanagam6753@gmail.com>
2021-07-09 09:26:18 +0530

    first commit

---
+++ mypro.rb
@@ -0,0 +1,14 @@
+// program on java
+public class Pavan{
+private int id;
+private string name;
+
+	Pavan( int id, String name){
+	this.id = id;
+	this.name=name;
+	}
+	public int getName()
+	return name;
+	public void setName(String name)
+	this.name= name;
+}
